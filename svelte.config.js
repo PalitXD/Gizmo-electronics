701aac7f3aa9318ec9f3fdeb55e602af1b6bbd03
PalitXD <73961134+PalitXD@users.noreply.github.com>
2023-01-04 21:15:59 +0000

    e

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adaptor-vercel';
-import { auto } from '@popperjs/core';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: auto()
-	},
-
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+		adapter: adapter()
+	}
 };
 
 export default config;
