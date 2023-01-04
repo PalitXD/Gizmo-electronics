bd19209c86e14cadd02ac3e68fc58b77e4069803
PalitXD <73961134+PalitXD@users.noreply.github.com>
2023-01-04 21:12:55 +0000

    e

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import vercel from '@sveltejs/adaptor-vercel';
+import { auto } from '@popperjs/core';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: vercel()
+		adapter: auto()
 	},
 
 	preprocess: [
